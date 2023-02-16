c96bfb2504f861f28a54e1c65f8959630e65bc15
Cristopher Mendoza <thekrisxp1@gmail.com>
2023-02-16 18:15:31 +0100

    Moving lights added to game scene

---
+++ Assets/Art/VFX/NoiseSway.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DistantLands
+{
+    public class NoiseSway : MonoBehaviour
+    {
+        [Tooltip("Controls the strength of the noise.")]
+        [SerializeField] private float depth = 1;
+        [Tooltip("Controls the distance between peaks of the noise.")]
+        [SerializeField] private float width = 1;
+        [Tooltip("Controls the seed of the noise. Leave 0 for a random seed.")]
+        [SerializeField] private int seed;
+        private Vector3 rot;
+
+        void Start()
+        {
+            if (seed == 0)
+                seed = Random.Range(0, 1000000);
+            rot = transform.localEulerAngles;
+
+        }
+
+        void Update()
+        {
+
+            float t = Time.time / width;
+
+            Vector3 offset = new Vector3(
+                Mathf.PerlinNoise(seed, t) - .5f,
+                Mathf.PerlinNoise(seed + 1000, t) - .5f,
+                Mathf.PerlinNoise(seed + 2000, t) - .5f);
+
+
+            transform.localEulerAngles = rot + (offset * depth);
+
+
+
+        }
+    }
+}
